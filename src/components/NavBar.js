6536f22d0333c675f7871a464c71bc6f87311697
Felix Kipkurui Langat <fexlangat@gmail.com>
2024-04-26 15:06:04 +0300

    list and keys

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,16 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      {/* Use map function to create <a> tag for each link */}
+      {links.map((link, index) => (
+        <a key={index} href={`#${link}`}>
+          {link}
+        </a>
+      ))}
+    </nav>
+  );
 }
 
 export default NavBar;
